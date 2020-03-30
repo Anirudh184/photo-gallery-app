4f1ac43d1e64c65318315b45a539431038cf45d1
Anirudha sah <anirudh.sah96@gmail.com>
2020-03-30 09:36:27 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -23,9 +23,10 @@ app.use(userRouter);
 // Serve static content
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('photo-gallery-client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'photo-gallery-client', 'build', 'index.html'));
+        // res.sendFile(path.resolve(__dirname, 'photo-gallery-client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../photo-gallery-client/build/index.html'));
     });
 }
 
