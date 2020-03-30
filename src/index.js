0c3a335d1a9b1233efbe11570c17f619f4bbe456
Anirudha sah <anirudh.sah96@gmail.com>
2020-03-30 09:22:14 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userRouter = require('../routes/user');
 const app = express(); 
+const path = require('path');
 
 require('../db/mongoose');
 const port = process.env.PORT;
@@ -15,9 +16,18 @@ app.use(express.json());
 app.use(userRouter); 
 
 
-app.use((req, res, next) => {
-    res.send("Express application");
-});
+// app.use((req, res, next) => {
+//     res.send("Express application");
+// });
+
+// Serve static content
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(port, () => {
     console.log(`Backend listening at port ${port}`);
