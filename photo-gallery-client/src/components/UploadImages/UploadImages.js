5db2da86b2e28e63b6eba45d7f40390062001330
Anirudha sah <anirudh.sah96@gmail.com>
2020-03-30 15:02:34 +0530

    change

--- photo-gallery-client/src/components/UploadImages/UploadImages.js
+++ photo-gallery-client/src/components/UploadImages/UploadImages.js
@@ -46,6 +46,7 @@ class UploadImages extends React.Component {
             currentState.statusText = 'Images Uploaded Succesfully'
             this.setState(currentState);
         }).catch(e => { 
+            console.log(e)
             currentState.statusText = e.error;
             this.setState(currentState); 
         }).finally(()=>{
